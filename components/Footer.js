66ec53a19224c162a037fed1ee981bd79cd755c0
Pedro Toribio <pedrom_t@hotmail.com>
2022-01-27 23:48:15 +0100

    [Footer] Added

--- components/Footer.js
+++ components/Footer.js
@@ -2,25 +2,28 @@ import { GlobeIcon } from "@heroicons/react/solid";
 
 function Footer() {
   return (
-    <footer className="grid w-full divide-y-[1px] divide-gray-300 bg-gray-100
-    text-sm text-gray-500">
+    <footer
+      className="grid w-full divide-y-[1px] divide-gray-300 bg-gray-100
+    text-sm text-gray-500"
+    >
       <div className="px-8 py-3">
         <p>República Dominicana</p>
       </div>
-      <div className="grid grid-cols-1">
-        <div className="">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 px-8 py-3  grid-flow-row-dense">
+        <div className="flex justify-center items-center md:col-span-2 lg:col-span-1
+        lg:col-start-2 ">
           <GlobeIcon className="h-5 mr-1 text-green-700" /> Carbon neutral since
           2007
         </div>
-        <div className="flex justify-center space-x-8 whitespace-nowrap">
-            <p>Advertising</p>
-            <p>Business</p>
-            <p>How Search Works</p>
+        <div className="flex justify-center space-x-8 whitespace-nowrap md:justify-self-start">
+          <p>Advertising</p>
+          <p>Business</p>
+          <p>How Search Works</p>
         </div>
-        <div className="">
-            <p>Privacy</p>
-            <p>Terms</p>
-            <p>Settings</p>
+        <div className="flex justify-center space-x-8 md:ml-auto">
+          <p>Privacy</p>
+          <p>Terms</p>
+          <p>Settings</p>
         </div>
       </div>
     </footer>
