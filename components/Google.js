2707793510cb22e1b559371e2986b3bea5c9cb54
Pedro Toribio <pedrom_t@hotmail.com>
2022-01-28 14:54:31 +0100

    [Bug] Let's build

--- components/Google.js
+++ components/Google.js
@@ -27,6 +27,7 @@ function Google() {
         src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
         height={100}
         width={300}
+        alt="Img"
       />
 
       <div
